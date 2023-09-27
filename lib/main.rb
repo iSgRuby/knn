205c31f44d5cddc659fb82bdea715710b0360f77
iSgRuby <manuel_31oct@outlook.com>
2023-09-27 04:04:40 -0600

    now you can add multiple points when executing knn function

--- lib/main.rb
+++ lib/main.rb
@@ -59,46 +59,57 @@ def add_to_chart(groups)
 end
 
 def knn(groups)
-  puts('Insert the coordinates of the point')
-  print('x: ')
-  x = gets.chomp.to_i
-  print('y: ')
-  y = gets.chomp.to_i
+  print('how many points?: ')
+  n = gets.chomp.to_i
+  puts('Insert the range of the points to generate')
+  print('from x: ')
+  x1 = gets.chomp.to_i
+  print('to x: ')
+  x2 = gets.chomp.to_i
+  print('from y: ')
+  y1 = gets.chomp.to_i
+  print('to y: ')
+  y2 = gets.chomp.to_i
+  points = create_points(n_points: n, cx1: x1, cy1: y1, cx2: x2, cy2: y2)
+
   print('Insert "K": ')
   k = gets.chomp.to_i
 
   knn_group = Groups.new('knn')
-  knn_group.add_points([Points.new(x, y)])
+  knn_group.add_points(points)
   groups << knn_group
   add_to_chart(groups)
   puts('check the chart and watch the new point with the name knn')
   puts('After you chek insert anything to continue')
   gets
 
-  points_distance = {}
-  groups.each do |group|
-    next if group == knn_group
+  points.each do |knn_point|
+    points_distance = {}
+    groups.each do |group|
+      next if group == knn_group
 
-    group.points.each { |point| points_distance[point] = Math.sqrt(((point.x - x)**2) + ((point.y - y)**2).abs) }
-  end
-  points_distance = Hash[points_distance.sort_by { |_key, value| value }.first(k)]
-
-  count = {}
-  points_distance.each do |key, value|
-    if count[key.group].nil?
-      count[key.group] = 1
-    else
-      count[key.group] += 1
+      group.points.each { |point| points_distance[point] = Math.sqrt(((point.x - knn_point.x)**2) + ((point.y - knn_point.y)**2).abs) }
     end
+    points_distance = Hash[points_distance.sort_by { |_key, value| value }.first(k)]
+
+    count = {}
+    points_distance.each do |key, value|
+      if count[key.group].nil?
+        count[key.group] = 1
+      else
+        count[key.group] += 1
+      end
+    end
+
+    group_to_add = count.max_by { |key, value| value }
+    group_to_add[0].add_points([knn_point])
+    knn_group.points.delete(knn_point)
+    groups.delete(knn_group) if knn_group.points.empty?
+    add_to_chart(groups)
+    puts('check the chart en watch how the point merged in another group')
+    puts('After you check insert anything to continue')
+    gets
   end
-
-  points_to_add = []
-  group_to_add = count.max_by{|key, value| value}.first
-  knn_group.points.each { |point| points_to_add << point }
-  group_to_add.add_points(points_to_add)
-
-  groups.delete(knn_group)
-
 end
 
 def two_groups(groups)
@@ -116,7 +127,7 @@ def menu
   puts '[1] Create a group'
   puts '[2] Add random points to a group'
   puts '[3] Load to chart'
-  puts '[4] Add one point knn'
+  puts '[4] Add points knn'
   puts '[5] generate 2 groups'
   puts '[0] Exit'
   print 'Insert an option: '
